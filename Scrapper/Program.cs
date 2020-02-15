c461d8bf053478a5040dcd4c04f98f803079a282
taras.hots <taras.hots@cdprojektred.com>
2020-02-15 12:53:31 +0100

    Initial commit

---
+++ Scrapper/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Net;
+using System.Linq;
+
+namespace Scrapper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("Need arguments!");
+            }
+
+            var pageReader = new HtmlWeb();
+            var page = pageReader.Load(args[0]);
+
+            var titleNode = page.DocumentNode.SelectSingleNode("//head/title");
+            Console.WriteLine("Reading " + titleNode.InnerText);
+
+            var textElement = page.DocumentNode.SelectSingleNode("//body/tr/td/font/div/div");
+
+            var dividedText = WebUtility.HtmlDecode(textElement.InnerText)
+                .Replace(",", "")
+                .Replace(".", "")
+                .Replace(":", "")
+                .Replace("\r\n", "")
+                .Replace("\r", "")
+                .Replace("\"", "")
+                .Split(' ');
+
+            var matchQuery = from word in dividedText
+                             group word by word.ToLower() into g
+                             orderby g.Count() descending
+                             select new { Word = g.Key, Count = g.Count() };
+
+            foreach (var nameGroup in matchQuery)
+            {
+                Console.WriteLine($"{nameGroup.Word} : {nameGroup.Count}");
+            }
+        }
+    }
+}
